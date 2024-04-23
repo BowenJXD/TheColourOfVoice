0783458c6eb2d9156357e34fa21fab13bc6d9e82
Inadeus <qinhuawen123@gmail.com>
2024-04-23 14:05:41 +0800

    Finish Benji Dialogue
    Finish Benji Dialogue

---
+++ TheColourOfVoice/Assets/Scripts/BigMap.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BigMap : MonoBehaviour
+{
+    [Header("Dialogue for Benj")]
+    public SequenceEventExecutor benj_preSequence;
+    public void StartDialogueSequence(SequenceEventExecutor sequenceEventExecutor) 
+    {
+        Debug.Log("Start Benji Dialogue");
+        if (sequenceEventExecutor == null) 
+        {
+            Debug.Log("Failure to excute Benji Dialogue");
+            return;
+        }
+
+        sequenceEventExecutor.Init(OnFinishedEvent);
+        sequenceEventExecutor.Excute();
+    }
+
+
+    void OnFinishedEvent(bool success)
+    {
+        Debug.Log(success);
+    }
+}
