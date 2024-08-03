d64c829164fa8e7ea08a2afe0d256608e5f847c6
Inadeus <qinhuawen123@gmail.com>
2024-08-03 18:30:10 +0800

    完成了开场的UI展开部分
    完成了开场的UI展开部分

--- TheColourOfVoice/Assets/Scripts/Levels/Level_PsyRoom.cs
+++ TheColourOfVoice/Assets/Scripts/Levels/Level_PsyRoom.cs
@@ -7,12 +7,53 @@ using UnityEngine;
 public class Level_PsyRoom : MonoBehaviour
 {
     public GameObject player;
-    private void Start()
+    public Camera uiCamera;
+    public GameObject mainPanel;
+    private int shakeCount = 0;
+    private bool playerIsAwake = false;
+    private void Update()
     {
-        /*player.transform.DOShakeRotation(1f, new Vector3(0.1f, 0.1f, 0), 10, 180, false)
-            .SetLoops(-1, LoopType.Incremental);*/
-        
-        player.transform.DOShakePosition(1f, new Vector3(0.1f, 0.1f, 0f), 10, 180, false)
+        if (Input.GetKeyDown(KeyCode.Space) && playerIsAwake == false)
+        {
+            Shake(player, 0.1f, 0.1f);
+            shakeCount++;
+
+            if (shakeCount == 5)
+            {
+                LittleWitchAwake();
+            }
+        }
+    }
+
+    private void LittleWitchAwake()
+    {
+        playerIsAwake = true;
+        if (!uiCamera)
+        {
+            return;
+        }
+
+        uiCamera.transform.DOMove(new Vector3(3.31f,0.63f,-10f), 1f).SetEase(Ease.InOutSine)
+            .onComplete = OnCameraMoveFinished;
+
+    }
+
+    private void Shake(GameObject target,float powerX, float powerY)
+    {
+        target.transform.DOShakePosition(1f, new Vector3(powerX, powerY, 0f), 10, 180, false)
             .SetLoops(1, LoopType.Incremental);
+    }
+    
+    private void OnCameraMoveFinished()
+    {
+        Debug.Log("Camera move finished");
+        if (mainPanel)
+        {
+           
+            mainPanel.SetActive(true);
+            RectTransform rectTransform = mainPanel.GetComponent<RectTransform>();
+            rectTransform.DOAnchorPos(new Vector2(1524.18f, -535.85f), 1f).SetEase(Ease.InOutSine);
+            rectTransform.DOSizeDelta(new Vector2(726.76f,964.2f),1f).SetEase(Ease.InOutSine);
+        }
     }
 }
