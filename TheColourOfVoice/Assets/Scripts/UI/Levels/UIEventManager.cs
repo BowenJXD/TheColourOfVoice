1341643403285fc7c2dfd291a41ff374a8f77881
Inadeus <qinhuawen123@gmail.com>
2024-08-19 14:09:54 +0800

    Fix bug that UI cant move
    Fix bug that UI cant move

--- TheColourOfVoice/Assets/Scripts/UI/Levels/UIEventManager.cs
+++ TheColourOfVoice/Assets/Scripts/UI/Levels/UIEventManager.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class GameEventManager : MonoBehaviour
 {
     [SerializeField] GameObject PauseUI;
-
+    private bool isUImoving = false;
     private void Awake()
     {
         LeanTween.reset();
@@ -22,37 +22,46 @@ public class GameEventManager : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             /*if (Time.timeScale == 0 && !PauseUI.gameObject.activeSelf) return;*/ // prevent opening the spell panel when the game is paused
-            if (!PauseUI.gameObject.activeSelf)
+            if (!PauseUI.gameObject.activeSelf && !isUImoving)
             {
                 Debug.Log("PausePanel");
                 PausePanelStart();
             }
-            else
+            else if (!isUImoving)
             {
                 Debug.Log("PausePanelEnd");
-               PausePanelEnd();
+                PausePanelEnd();
             }
-            
         }
     }
 
 
     void PausePanelStart()
     {
+        isUImoving = true;
         Debug.Log("PausePanel");
         Time.timeScale = 0;
         PauseUI.gameObject.SetActive(true);
-        //LeanTween.moveLocal(PauseUI, new Vector3(0f, -20f, 0f), 1f).setIgnoreTimeScale(true).setEase(LeanTweenType.easeInOutBack);
+        //LeanTween.moveLocal(PauseUI, new Vector3(0f, -20f, 0f), 1f).setIgnoreTimeScale(true);
         PauseUI.TryGetComponent(out RectTransform rectTransform);
-        rectTransform.anchoredPosition = new Vector2(0, 0);
+        //rectTransform.anchoredPosition = new Vector2(0, 0);
+        rectTransform.DOAnchorPos(new Vector2(0,0),1f).SetEase(Ease.OutBounce).onComplete = () => isUImoving = false;
     }
 
     void PausePanelEnd()
     {
-        LeanTween.moveLocal(PauseUI, new Vector3(0f, 400f, 0f), 1f).setIgnoreTimeScale(true).setOnComplete(() =>
+        isUImoving = true;
+        /*LeanTween.moveLocal(PauseUI, new Vector3(0f, 400f, 0f), 1f).setIgnoreTimeScale(true).setOnComplete(() =>
         {
             PauseUI.gameObject.SetActive(false);
             Time.timeScale = 1; 
-        });
+        });*/
+        PauseUI.TryGetComponent(out RectTransform rectTransform);
+        rectTransform.DOAnchorPos(new Vector2(0,400),1f).SetEase(Ease.OutBounce).onComplete = () =>
+        {
+            PauseUI.gameObject.SetActive(false);
+            Time.timeScale = 1;
+            isUImoving = false;
+        };
     }
 }
