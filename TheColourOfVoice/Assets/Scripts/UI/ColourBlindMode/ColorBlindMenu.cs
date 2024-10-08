69a08652a23660140902a94d4d2888e3a6bd16bc
BowenJXD <aaronjiang12138@gmail.com>
2024-10-08 14:38:44 +1100

    Added colour blind setting scene.

---
+++ TheColourOfVoice/Assets/Scripts/UI/ColourBlindMode/ColorBlindMenu.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ColourBlindMenu : MonoBehaviour
+{
+    public string SceneName;
+    public GameObject GlobalVolume;
+    private Slider[] Sliders;
+
+    private void Start()
+    {
+        Sliders = GetComponentsInChildren<Slider>();
+    }
+
+    public void MenuButton()
+    {
+        Destroy(GlobalVolume);
+        SceneManager.LoadScene(SceneName);
+    }
+
+    public void ResetButton()
+    {
+        for (int i = 0; i < Sliders.Length; i++)
+        {
+            Sliders[i].value = (float)i / Sliders.Length;
+        }
+    }
+}
